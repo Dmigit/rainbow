0a16b1ce0568cabdc319c86ad2959361b3578a98
moxey.eth <jakemoxey@gmail.com>
2022-05-31 15:23:48 +1000

    improve intro screen load performance

--- src/hooks/useENSResolveName.ts
+++ src/hooks/useENSResolveName.ts
@@ -4,9 +4,21 @@ import {
   saveResolveName,
 } from '@rainbow-me/handlers/localstorage/ens';
 import { web3Provider } from '@rainbow-me/handlers/web3';
+import { queryClient } from '@rainbow-me/react-query/queryClient';
+
+export const ensResolveNameQueryKey = (ensName: string) => [
+  'resolve-name',
+  ensName,
+];
+
+export async function prefetchENSResolveName(ensName: string) {
+  await queryClient.prefetchQuery(ensResolveNameQueryKey(ensName), async () =>
+    web3Provider.resolveName(ensName)
+  );
+}
 
 export default function useENSResolveName(ensName: string) {
-  return useQuery(['resolve-name', ensName], async () => {
+  return useQuery(ensResolveNameQueryKey(ensName), async () => {
     const cachedAddress = await getResolveName(ensName);
     if (cachedAddress) return cachedAddress;
 
